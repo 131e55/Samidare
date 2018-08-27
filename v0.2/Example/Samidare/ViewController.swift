80bf2678a3150f7aaa7e3f95fbb711b7b3821b3f
Keisuke Kawamura <131e550807@gmail.com>
2018-08-27 16:39:45 +0900

    Add New Samidare project files

---
+++ v0.2/Example/Samidare/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Samidare
+//
+//  Created by Keisuke Kawamura on 08/27/2018.
+//  Copyright (c) 2018 Keisuke Kawamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
