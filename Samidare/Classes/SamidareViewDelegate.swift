5edc8f98ee630d18bdd13d21903990ab0d04ad15
Keisuke Kawamura <131e550807@gmail.com>
2019-09-30 07:42:09 +0900

    Delegate 案

---
+++ Samidare/Classes/SamidareViewDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SamidareViewDelegate.swift
+//  Samidare
+//
+//  Created by Keisuke Kawamura on 2019/09/28.
+//
+
+import UIKit
+
+// TODO:
+// EditingDelegate
+// CreatingDelegate
+
+//public protocol SamidareViewDelegate: class {
+//    func didBeginEditingEvent(_ cell: EventCell, in samidareView: SamidareView)
+//    func didEditEvent(_ cell: EventCell, in samidareView: SamidareView)
+//    func didEndEditingEvent(_ cell: EventCell, in samidareView: SamidareView)
+//}
